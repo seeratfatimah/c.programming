f5bcae8ec95c4665015b22f5c2c7e94c1410553d
seeratfatimah <fatimahseeratt0@gmail.com>
2026-04-17 12:39:02 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,49 @@
+
+/*#include <stdio.h>
+int main() {
+   int n;
+   double x;
+   
+printf("enter terms");
+scanf("%d", &n);
+printf("enter the number");
+scanf("%lf", &x);
+printf("the square series is:\n");
+for (int i=1; i<=n; i++) {
+  printf("%.0lf\n", x);
+        x= x*x;
+
+
+
+  return 0;
+}
+
+}*/
+
+
+
+/*// factors of a number
+# include <stdio.h>
+ int main() {
+   int n;
+ printf("enter a number");
+ scanf("%d", &n);
+printf("the factors of %d are:\n", n);
+for (int i=1; i<=n; i++) {
+      if(n%i==0) {
+       printf("%d\n", i);
+     }
+
+
+ }
+
+
+
+ return 0;
+ }*/
+
+
+
+
+
+ 
