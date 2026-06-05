716a948a2f6949c85bd9e683863cb3cbeb24dacf
seeratfatimah <fatimahseeratt0@gmail.com>
2026-06-05 11:59:26 +0530

    Add files via upload

---
+++ fatima1234.c
@@ -0,0 +1,93 @@
+/*#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int num1, num2, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    result = sum(num1, num2);
+
+    printf("sum = %d", result);
+    return 0;
+}*/
+
+
+
+/*#include <stdio.h>
+
+int multiply(int a, int b)
+{
+    return a * b;
+
+}
+
+int main()
+{
+    int num1, num2, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    result = multiply(num1, num2);
+
+    printf("Product = %d", result);
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+/*#include <stdio.h>
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+int main()
+{
+    int num1, num2, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    result = subtract(num1, num2);
+
+    printf("Difference = %d", result);
+
+    return 0;
+}*/
+
+
+
+
+/*#include <stdio.h>
+
+float divide(float a, float b)
+{
+    return a / b;
+}
+
+int main()
+{
+    float num1, num2, result;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f",  &num1, &num2);
+
+    result = divide(num1, num2);
+
+    printf("Quotient = %.2f", result);
+
+    return 0;
+}*/
